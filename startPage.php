b5004faa9085d7f5f6ed9d693c90f8dd713ef059
Diana Sedman <dianasedman@gmail.com>
2024-03-14 21:07:38 +0200

    start page design, css

---
+++ startPage.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style/startPage.css">
+  <title>Pealeht</title>
+</head>
+
+<body>
+  <div class="container">
+    <div class="container-right">
+      <div class="button-container">
+        <a class="button-start" href="recommendation.php">SOOVITUSED</a>
+      </div>
+      <div class="image-container-right">
+        <img class="image-right" src="img/right-side.png" alt="Generated with AI ">
+      </div>
+    </div>
+    <div class="container-left">
+      <div class="button-container">
+        <a class="button-start" href="nsaid-calculator.php">KALKULAATOR</a>
+      </div>
+      <div class="image-container-left">
+        <img class="image-left" src="img/left-side.jpg" alt="Generated with AI ">
+      </div>
+    </div>
+  </div>
+
+
+
+</body>
+
+</html>
